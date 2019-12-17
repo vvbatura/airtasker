775e39c275af711ff952ee933f2515c19ad5d6dc
viktor <vvbatura@gmail.com>
2019-12-17 16:54:47 +0200

    task profile skill

--- routes/api.php
+++ routes/api.php
@@ -51,20 +51,5 @@ Route::group(['namespace' => 'Api'], function () {
 
     });
 
-    /*Route::group(['middleware' => 'jwt'], function() {
-
-        Route::get('/countries', 'CountryController');
-        Route::get('/categories', 'CategoryController');
-        Route::get('/cities', 'CityController');
-        Route::get('/experiences', 'ExperienceController');
-        Route::resource('/vacancies', 'VacancyController');
-        Route::put('/users', 'UserController@update')->middleware('jwt');
-        Route::post('/users/password', 'UserController@changePassword')->middleware('jwt');
-
-        Route::get('/profiles', 'ProfileController@show');
-        Route::put('/profiles/{profile}', 'ProfileController@update');
-
-    });*/
-
 
 });
