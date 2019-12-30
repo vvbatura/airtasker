2780bb0f114abeff4a4848e45c8b030b46d11ae0
Taras Vitovshchyk <vitovstreet@gmail.com>
2019-12-30 17:43:57 +0200

    fix register

--- resources/js/layouts/ProfileLayout.vue
+++ resources/js/layouts/ProfileLayout.vue
@@ -8,22 +8,32 @@
                         <div class="img_size">
                             <img src="img/blog-1.jpg" alt=""/>
                         </div>
-                        <p class="name_user">John Smith</p>
+                        <p class="name_user text-center">John Smith</p>
                     </div>
                     <ul class="dash_list_route">
-                        <router-link to="/dashboard">
-                            Dasboard
-                        </router-link>
-                        <router-link to="/payments-history">
-                            Payments history
-                        </router-link>
-                        <router-link to="/payment-method">
-                            Payment methods
-                        </router-link>
-                        <router-link to="/notifications">
-                            Notifications
-                        </router-link>
-                        <a href="/" class="dropdown">Settings</a>
+                        <li>
+                            <router-link to="/dashboard">
+                                Dasboard
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/payments-history">
+                                Payments history
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/payment-method">
+                                Payment methods
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/notifications">
+                                Notifications
+                            </router-link>
+                        </li>
+                        <li>
+                            <a href="/" class="dropdown">Settings</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="profile_content">
@@ -64,10 +74,12 @@ export default {
     padding-top: 58px;
 }
 .img_name {
+    padding-top: 30px;
+    margin-bottom: 25px;
     .img_size {
         width: 130px;
         height: 130px;
-        margin-bottom: 10px;
+        margin: 0 auto 10px auto;
         border-radius: 50%;
         overflow: hidden;
         img {
@@ -85,9 +97,24 @@ export default {
     padding-left: 210px;
     width: 100%;
 }
-.img_name {
-    padding-top: 30px;
-    margin: 0 auto;
+.dash_list_route {
+    padding-left: 15px;
+    li {
+        a {
+            color: #545a77;
+            height: 44px;
+            display: block;
+            transition: .3s linear;
+            &:hover {
+                text-decoration: none;
+                color: #292b32;
+                transition: .3s linear;
+            }
+        }
+    }
+}
+.sidebar_box {
+    height: calc(100vh - 58px);
 }
 </style>
 
--- resources/js/pages/Profile/Dashboard.vue
+++ resources/js/pages/Profile/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="flow_box">
         <h2>Dashboard</h2>
     </div>
 </template>
@@ -11,5 +11,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+.flow_box {
+    border-left: 1px solid #bbc2dc;
+    box-shadow: 1px 0 0 0 rgba(0,0,0,.1);
+    //min-height: calc(100vh - 58px);
+}
 </style>
